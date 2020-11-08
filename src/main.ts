ddd7b02dea9510e050c8bf4cf673a823b62c6822
pchmn <pchmn.dev@gmail.com>
2020-11-08 03:14:37 +0100

    Project configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: configService.get('microServiceTransport'),
+    options: {
+      host: 'localhost',
+      port: configService.get('port')
+    }
+  })
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(configService.get('microservicePort'));
+  Logger.log(`🚀 Auth API is running`, 'Bootstrap');
 }
 bootstrap();
